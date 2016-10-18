8fd29f56cc21af790cc2b5b84f36fe72b64b5829
Carl Nordenfelt <carl@nordenfelt.com>
2016-10-18 12:03:38 +0200

    Fixed typo in error message

--- src/lib/file-helper.js
+++ src/lib/file-helper.js
@@ -8,7 +8,7 @@ pub.read = function read(file) {
     try {
         fs.accessSync(file);
     } catch (error) {
-        console.log('File not be found or insufficient permissions to read', file);
+        console.log('File not found or insufficient permissions to read', file);
         throw error;
     }
     return fs.readFileSync(file).toString();
